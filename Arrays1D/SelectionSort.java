944947ae534f146a974c85c58afe0d43ff915546
Achintya1205 <168367399+Achintya1205@users.noreply.github.com>
2025-01-14 19:04:34 +0530

    Add files via upload

---
+++ Arrays1D/SelectionSort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5,7,1,11,43,22,2,77,61,6};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+}
